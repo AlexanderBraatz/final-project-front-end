f001dd4966429d6d802ea032290122f1a4f93393
Alexander Braatz <alex.braatz.g@gmail.com>
2020-09-17 15:45:19 +0100

    Updates

---
+++ precache-manifest.b5fc1edc83c343936f87daa7340314bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e5814c87a63cfe7661662bbc0602dd",
+    "url": "/final-project-front-end/index.html"
+  },
+  {
+    "revision": "1f7cc9c46d8e17edf329",
+    "url": "/final-project-front-end/static/css/main.ebf8224d.chunk.css"
+  },
+  {
+    "revision": "e7bf551062247c753190",
+    "url": "/final-project-front-end/static/js/2.e2785707.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/final-project-front-end/static/js/2.e2785707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f7cc9c46d8e17edf329",
+    "url": "/final-project-front-end/static/js/main.72eacaee.chunk.js"
+  },
+  {
+    "revision": "68fa2e9e04daf6804705",
+    "url": "/final-project-front-end/static/js/runtime-main.346ac0dc.js"
+  },
+  {
+    "revision": "49365a533e8b0b687ea89e8b06a7f64c",
+    "url": "/final-project-front-end/static/media/Programming.49365a53.jpg"
+  },
+  {
+    "revision": "a2905bc7d245a36d255960b4439b74c7",
+    "url": "/final-project-front-end/static/media/purplecode.a2905bc7.jpg"
+  }
+]);
