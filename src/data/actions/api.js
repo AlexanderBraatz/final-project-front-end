84481c71e03514cf482e16233229923df0ef42fb
Alexander Braatz <alex.braatz.g@gmail.com>
2020-09-18 11:05:41 +0100

    fixed search to not dispach actions if no data is returned

--- src/data/actions/api.js
+++ src/data/actions/api.js
@@ -54,8 +54,10 @@ export const sendSearch = (value) => {
 
     return (dispatch) => {
         axios.get(`/search?query=${value}`).then(({ data }) => {
-            dispatch(setWordCard(data.data[0]));
-            history.push(`/words/${data.data[0].id}`)
+            if(data.data[0]){
+                dispatch(setWordCard(data.data[0]));
+                history.push(`/words/${data.data[0].id}`)
+            }
         });
     };
 };
