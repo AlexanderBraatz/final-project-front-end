efc5a2ff6d1f2ae8db21f8e2378e04044a2891f8
Rohan <rohanharris1@outlook.com>
2020-09-18 14:36:26 +0100

    styling change to loading bar

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -12,12 +12,13 @@ class Loading extends Component {
         const { children, loaded } = this.props;
 
         return loaded ? children : (
-            <div className="progress">
-                <div
-                    className="progress-bar progress-bar-striped active"
-                    style={{ width: "100%" }}
-                />
-            </div>
+            <>
+            <body className="body">
+                <div className="progress-border">
+                    <div className="progress">Loading....</div>
+                </div>
+            </body>
+            </>
         );
     }
 }
