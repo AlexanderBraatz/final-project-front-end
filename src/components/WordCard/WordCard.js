b8c7bc094e2f925ffb35213c13909bc185bf114f
Rohan <rohanharris1@outlook.com>
2020-09-18 11:19:05 +0100

    styling changes added

--- src/components/WordCard/WordCard.js
+++ src/components/WordCard/WordCard.js
@@ -8,16 +8,18 @@ import WordLinks from './WordLinks';
 const WordCard = ({id, word, definition, liked, links}) =>  { 
     return(
         <>
+        <div className='container'>
         <div className='word-card'>
         <h2 className='word-heading'>{ word }</h2>
         
         
-            <p className= "intro"> {definition} </p>
+            <p className= "word-define"> {definition} </p>
 
             <WordLinks links={links} />
 
             <Heart id={ id } liked={ liked } />
             
+        </div>
         </div>
         </>
     )
--- src/components/WordCard/WordLinks.js
+++ src/components/WordCard/WordLinks.js
@@ -4,10 +4,14 @@ import React  from 'react';
 const WordLinks = ({ links }) => (
     
     <span >
+        
         {links.map(link => (
+            <>
             <a 
+            className="list-items-nav"
             key={ link.id } 
             href={ link.link }>{ link.link }</a>
+            </>
         ))}
     </span>
 );
